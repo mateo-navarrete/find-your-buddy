e31b1d335076d47f488b41c870a11dbc036788de
Muna Sharma <munasharma@pursuit.org>
2019-03-27 15:38:15 -0400

    changed files name to match master

--- src/js/components/HomePage.js
+++ src/js/components/HomePage.js
@@ -2,24 +2,22 @@ import React from "react";
 import { withAnimals } from "../containers";
 import { PetImgs } from "./petImgs";
 
-    export const HomePage = props => {
-      return (
-        <>
+export const Home = props => {
+  return (
+    <>
+      <div>
+        <div>
+          <h1> Find your pet</h1>
+        </div>
+        <div>
+          {" "}
+          <p>description </p>
           <div>
-            <div>
-              <h1> Find your pet</h1>
-            </div>
-            <div>
-              {" "}
-              <p>description </p>
-              <div>
-                <PetImgs />
-              </div>
-            </div>
+            <PetImgs />
           </div>
-        </>
-      );
-    };
+        </div>
+      </div>
+    </>
   );
 };
 
