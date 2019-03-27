b4d2d256e4fd9481c37050f4d288cb36f82a69fa
mateo-navarrete <mateonavarrete@pursuit.org>
2019-03-27 17:03:53 -0400

    updated Form components

--- src/js/components/Form/Household.js
+++ src/js/components/Form/Household.js
@@ -4,6 +4,21 @@ export const Household = props => {
   return (
     <>
       <div>Household</div>
+      <div>People living in your household:</div>
+      <div>Name </div>
+      <div>Contact Number </div>
+      <div>Relationship </div>
+      <div>Age</div>
+      <div>Type of Residence: </div>
+      <div>Do you rent or own? ___ </div>
+      <div>Type of Home: House ___</div>
+      Apartment/Condo ___ Other:_____________________
+      <div>
+        If Apartment/Condo, what are the rules about pets?
+        _______________________________________________
+      </div>
+      <div>If you rent, please list landlord name and phone number:____</div>
+      <br />
     </>
   );
 };
