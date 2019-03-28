50fc65d050d32d35555715456a05dc3cdf2cb8c6
JJ <jackyong@pursuit.org>
2019-03-28 01:51:02 -0400

    Logo and CSS styling

--- src/js/components/Petpages/animalDisplay.js
+++ src/js/components/Petpages/animalDisplay.js
@@ -27,8 +27,7 @@ export const AnimalDisplay = ({ included, data }) => {
             
             <div className = "animal_info">
            
-
-            <p> {pet.attributes.name} </p>
+            <p className = "animal_name"> {pet.attributes.name} </p>
             {pet.attributes.sex === "Female" ? <div className = "sex"> <img  src="https://img.icons8.com/material/24/000000/female.png" /> </div> : <div className = "sex" ><img src="https://img.icons8.com/material/24/000000/male.png"/></div>}
            {pet.meta.ageString ? <p> {pet.meta.ageString}</p> : null}
             <p> {pet.meta.breedPrimary} </p>
