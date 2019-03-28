5c03bb9648cbd91a96fd9d11e85881e29f31e99b
Thomas Perez <thomasperez@pursuit.org>
2019-03-27 20:42:04 -0400

    sending som home page things

--- src/js/components/NavBar.js
+++ src/js/components/NavBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/logo.css";
 import "../../styles/navBar.css";
-import "../../image/fyblogo.png";
+import logo from "../../image/fyblogo.png";
 
 export const NavBar = props => {
   return (
@@ -11,7 +11,7 @@ export const NavBar = props => {
         <Link to="/">
           <div className="flex align">
             <div className="logoDiv">
-              <img src="../../image/fyblogo.png" alt="" />
+              <img src={logo} alt="" />
             </div>
           </div>
         </Link>
